206a88bd064db358eaf6117e805bb82e11d53919
escape0707 <tothesong@gmail.com>
2022-02-24 16:55:48 +0800

    Squashed commit of the following:
    commit 3caf2d4420c046f276587171f1ddc623e69cc993
    Author: escape0707 <tothesong@gmail.com>
    Date:   Thu Feb 24 16:28:36 2022 +0800
    
        move files
    
    commit e2e40fb4b961243165f6cb94362088af78753588
    Author: escape0707 <tothesong@gmail.com>
    Date:   Tue Apr 20 13:43:22 2021 +0800
    
        Use more general pronunciation pattern
    
    commit df304bbf260e7e68242ea1c4b9c6825ea91b61e2
    Author: escape0707 <tothesong@gmail.com>
    Date:   Sun Apr 11 00:57:54 2021 +0800
    
        Freeze
    
    commit 5b5a9afe23bfe06444488614c37fd84d0fd6d17f
    Author: escape0707 <tothesong@gmail.com>
    Date:   Sun Apr 11 00:38:15 2021 +0800
    
        Better
    
    commit de962ad40a9e65e72e4659fdbe252badcc667b94
    Author: escape0707 <tothesong@gmail.com>
    Date:   Sun Apr 11 00:18:57 2021 +0800
    
        Working
    
    commit a615f6c2141395c8f3f7f5659a305003c890ccd9
    Author: escape0707 <tothesong@gmail.com>
    Date:   Sat Apr 10 23:55:48 2021 +0800
    
        Bearly working
    
    commit 2cda51f33dac71e7d257b05a5d72a53b319ef894
    Author: escape0707 <tothesong@gmail.com>
    Date:   Sat Apr 10 16:06:44 2021 +0800
    
        Fix
    
    commit ac9474fbde4675b51bd353ebff5daed015b50cc0
    Author: escape0707 <tothesong@gmail.com>
    Date:   Sat Apr 10 15:48:24 2021 +0800
    
        Change to simple import
    
    commit 89f07c7f94767f85fe542d450ec9e495332bbdb6
    Author: escape0707 <tothesong@gmail.com>
    Date:   Sat Apr 10 15:42:52 2021 +0800
    
        Update get pronunciation
    
    commit 5b43d73f2b2d6aaa4204aba670276522f6c681a6
    Author: escape0707 <tothesong@gmail.com>
    Date:   Sat Apr 10 15:31:19 2021 +0800
    
        Add get_html_following_link
    
    commit 3e8e5a8e78fdd527c1d521ea4eba6b8016e6cf24
    Author: escape0707 <tothesong@gmail.com>
    Date:   Sat Apr 10 12:56:13 2021 +0800
    
        Update Enum definition
    
    commit d7000c9ff392352741e93ba6dbfa91b8b7014749
    Author: escape0707 <tothesong@gmail.com>
    Date:   Sat Apr 10 12:41:10 2021 +0800
    
        Add PHON
    
    commit 9dc5a4b6d3576e000f66e36145c907a22568c993
    Author: escape0707 <tothesong@gmail.com>
    Date:   Sat Apr 10 12:40:56 2021 +0800
    
        Rename methods
    
    commit 8554665507211e0695b1777dfe83c042e9b6ccac
    Author: escape0707 <tothesong@gmail.com>
    Date:   Sat Apr 10 12:38:43 2021 +0800
    
        Implement raw html logger
    
    commit 5a4868a0d7a62ea1691f863b86152bb810844935
    Author: escape0707 <tothesong@gmail.com>
    Date:   Sat Apr 10 12:36:58 2021 +0800
    
        Use Enum for dialect
    
    commit a27c287b6201948cec61e0135e79152a13407737
    Author: escape0707 <tothesong@gmail.com>
    Date:   Fri Apr 9 22:57:13 2021 +0800
    
        Revise globals
    
    commit b9952674784037020a445baeca1af70eaef11447
    Author: escape0707 <tothesong@gmail.com>
    Date:   Fri Apr 9 20:04:53 2021 +0800
    
        Rename dictionary class
    
    commit 7b5171d8c078369697abd8479d74ae7e9c252d9e
    Author: escape0707 <tothesong@gmail.com>
    Date:   Fri Apr 9 18:13:00 2021 +0800
    
        Format using black
    
    commit bf2034f16504c7dc0c3f16d76c7466f81d99eb32
    Author: escape0707 <tothesong@gmail.com>
    Date:   Fri Apr 9 18:04:42 2021 +0800
    
        Rename OALD9 to OALD10
    
    commit c1498df43d434d52e96e0f01f20047179f25f14e
    Author: escape0707 <tothesong@gmail.com>
    Date:   Fri Apr 9 18:04:26 2021 +0800
    
        Import OALD9
    
    commit 8611ef5d34a0e1b4ffa82d3137ff5553dbf8a1e9
    Author: escape0707 <tothesong@gmail.com>
    Date:   Fri Apr 9 17:41:09 2021 +0800
    
        Add config
    
    commit 0798b14319b22f6f4c47ee5e2fe919eeddef8336
    Author: escape0707 <tothesong@gmail.com>
    Date:   Fri Apr 9 17:35:59 2021 +0800
    
        Initial commint

---
+++ addons21/fastwq/service/dict/OALD10.py
@@ -0,0 +1,201 @@
+# -*- coding:utf-8 -*-
+import enum
+import pathlib
+import random
+import re
+import traceback
+from typing import Optional
+
+from bs4 import BeautifulSoup
+from bs4.element import Tag
+
+from ..base import *
+
+
+class Dialect(enum.Enum):
+    BRITISH = "gb"
+    AMERICAN = "us"
+
+
+PRONUNCIATION_PATTERN_TEMPLATE = r"sound://(\w*?_{dialect}_\w*?.mp3)"
+PATTERN_BY_DIALECT_COLLECTION = {
+    dialect: re.compile(PRONUNCIATION_PATTERN_TEMPLATE.format(dialect=dialect.value))
+    for dialect in Dialect
+}
+DICT_PATH: Optional[str] = None
+PHONETIC_PATTERN = re.compile(r'<span class="phon">/(.*?)/</span>')
+
+
+# Register the dict service with a labels to be shown in the dicts list.. Chinese first, English second.
+@register(["本地词典-牛津高阶10", "MDX-OALD10"])
+class OALD10(MdxService):
+    def __init__(self):
+        dict_path = DICT_PATH
+        if not dict_path:
+            # if auto detect DICT_PATH
+            from ...service import service_manager, service_pool
+
+            for clazz in service_manager.mdx_services:
+                service = service_pool.get(clazz.__unique__)
+                title = service.builder._title if service and service.support else ""
+                service_pool.put(service)
+                if title.startswith("牛津高阶英汉双解词典（第10版）V3"):
+                    dict_path = service.dict_path
+                    break
+        super(OALD10, self).__init__(dict_path)
+
+    @property
+    def title(self) -> str:
+        return getattr(self, "__register_label__", self.unique)
+
+    @export("HTML")
+    def just_raw_html(self) -> str:
+        html = self.get_html()
+        # (pathlib.Path.home() / "workspaces/lookup.html").write_text(
+        #     html, encoding="utf-8"
+        # )
+        return html
+
+    @export("PHON")
+    def why_i_cant_use_fld_phonetic(self) -> str:  # TODO
+        html = self.get_html()
+        m = PHONETIC_PATTERN.search(html)
+        return f"[{m[1]}]" if m else ""
+
+    @export("BRE_PRON")
+    def field_pronunciation_british(self):
+        return self._field_pronunciation(Dialect.BRITISH)
+
+    @export("AME_PRON")
+    def field_pronunciation_american(self):
+        return self._field_pronunciation(Dialect.AMERICAN)
+
+    @export("All examples with audios")
+    def fld_sentence_audio(self):
+        return self._range_sentence_audio([i for i in range(0, 100)])
+
+    @export("Random example with audio")
+    def fld_random_sentence_audio(self):
+        return self._range_sentence_audio()
+
+    @export("First example with audio")
+    def fld_first1_sentence_audio(self):
+        return self._range_sentence_audio([0])
+
+    @export("First 2 examples with audios")
+    def fld_first2_sentence_audio(self):
+        return self._range_sentence_audio([0, 1])
+
+    # Recursively jump to the real definition that current word page links to
+    def _get_html_following_link(self) -> str:
+        html = self.get_html()
+        while html.startswith("@@@LINK="):
+            self.word = html[8:]
+            html = self.get_html()
+        return html
+
+    @export("DEF")
+    def fld_definition(self):
+        try:
+            html = self._get_html_following_link()
+            # (pathlib.Path.home() / "workspaces/lookup.html").write_text(
+            #     html, encoding="utf-8"
+            # )
+            soup = parse_html(html)
+            entry_collection = soup.select("div.entry")
+            definition_html = BeautifulSoup("<div></div>", "html.parser")
+            word_definition = definition_html.div
+            for entry in entry_collection:
+                # Extract word top level info
+                top_tag = definition_html.new_tag("div")
+                word_definition.append(top_tag)
+                top = entry.contents[0]
+                top_selected = top.select("span:is(.pos, .grammar, .labels)")
+                top_string = " ".join(map(Tag.get_text, top_selected))
+                top_tag.string = top_string
+                # Extract each sense of the word
+                sense_list_tag = definition_html.new_tag("ol")
+                word_definition.append(sense_list_tag)
+                sense_collection = entry.contents[1]
+                for sense in sense_collection.select("li.sense"):
+                    # First, remove info other than definition
+                    for etc in sense.select("span.ox-enlarge-label"):
+                        etc.decompose()
+                    english_definition = sense.find("span", class_="def")
+                    chinese_definition = sense.find("deft")
+                    last_wanted = chinese_definition or english_definition
+                    while last_wanted is not sense:
+                        for examples_etc in last_wanted.find_next_siblings():
+                            examples_etc.decompose()
+                        last_wanted = last_wanted.parent
+                    # Insert <li> into <ol>
+                    sense_tag = definition_html.new_tag("li")
+                    sense_list_tag.append(sense_tag)
+                    sense_tag.string = sense.get_text(" ", strip=True)
+            return definition_html.prettify()
+        except Exception:
+            return traceback.format_exc()
+
+    def _field_pronunciation(self, dialect: Dialect) -> str:
+        """获取发音字段"""
+        html = self.get_html()
+        pronunciation_pattern = PATTERN_BY_DIALECT_COLLECTION[dialect]
+        match = pronunciation_pattern.search(html)
+        if match is None:
+            return ""
+        in_mdd_path = "/" + match[1]  # upstream don't use pathlib for this
+        extract_to_path = pathlib.Path(f"OALD10-{self.word}.mp3")
+        self.save_file(in_mdd_path, extract_to_path)
+        # get anki's full save path of the file and return it
+        return self.get_anki_label(extract_to_path, "audio")
+
+    def _range_sentence_audio(self, range_arr=None):
+        m = self.get_html()
+        if m:
+            soup = parse_html(m)
+            el_list = soup.findAll(
+                "x-g-blk",
+            )
+            if el_list:
+                maps = []
+                for element in el_list:
+                    sounds = element.find_all("a")
+                    if sounds:
+                        br_sound = "None"
+                        us_sound = None
+                        en_text = cn_text = ""
+                        for sound in sounds:
+                            if sound.find(["audio-gbs-liju", "audio-brs-liju"]):
+                                br_sound = sound["href"][7:]
+                            elif sound.find(["audio-uss-liju", "audio-ams-liju"]):
+                                us_sound = sound["href"][7:]
+                        try:
+                            en_text = element.x["wd"]
+                            cn_text = element.x.chn.contents[1]
+                        except:
+                            continue
+                        if us_sound:  # I mainly use us_sound
+                            maps.append([br_sound, us_sound, en_text, cn_text])
+
+            my_str = ""
+            range_arr = (
+                range_arr if range_arr else [random.randrange(0, len(maps) - 1, 1)]
+            )
+            if maps:
+                for i, e in enumerate(maps):
+                    if i in range_arr:
+                        br_sound = e[0]
+                        us_sound = e[1]
+                        en_text = e[2]
+                        cn_text = e[3]
+                        us_mp3 = self._fld_audio(us_sound)
+                        if br_sound != "None":
+                            br_mp3 = self._fld_audio(br_sound)
+                        else:
+                            br_mp3 = ""
+                        # please modify the code here to get br_mp3
+                        # my_str = my_str + br_mp3 + ' ' + en_text  + cn_text + '<br>'
+                        # my_str = my_str + us_mp3 + en_text  + cn_text + '<br>'
+                        my_str = my_str + br_mp3 + us_mp3 + en_text + cn_text + "<br>"
+            return my_str
+        return ""
